82084e10089d73855e1d788f21b4e6d62f54fd93
Ömer ALTAN <omeraltan66@gmail.com>
2022-06-26 14:01:52 +0300

    Update Project

---
+++ src/components/AddCar.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const AddCar = () => {
+  const [open, setOpen] = useState(false);
+  const [car, setCar] = useState({
+    brand: '',
+    model: '',
+    color: '',
+    year: '',
+    fuel: '',
+    price: '',
+  });
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleChange = event => {
+    setCar({ ...car, [event.target.name]: event.target.value });
+  };
+  return (
+    <div>
+      <button onClick={handleClickOpen}>New Car</button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>New car</DialogTitle>
+        <DialogContent>
+          <input placeholder="Brand" name="brand" value={car.brand} onChange={handleChange} />
+          <br />
+          <input placeholder="Model" name="model" value={car.model} onChange={handleChange} />
+          <br />
+          <input placeholder="Color" name="color" value={car.color} onChange={handleChange} />
+          <br />
+          <input placeholder="Year" name="year" value={car.year} onChange={handleChange} />
+          <br />
+          <input placeholder="Price" name="price" value={car.price} onChange={handleChange} />
+          <br />
+        </DialogContent>
+        <DialogActions>
+          <button onClick={handleClose}>Cancel</button>
+          <button onClick={handleClose}>Save</button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddCar;
